d80807a8b5f263df3324d554b6ced3d0ee44beb2
beataj <beata@emc2wave.com>
2021-03-23 18:07:55 -0700

    add secondsPerSolution

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -27,10 +27,18 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const startTime = new Date();
+    let numberSolved = 0;
+
     this.mathForm.statusChanges.pipe(
       filter(value => value === 'VALID'),
       delay(300)
     ).subscribe(() => {
+      numberSolved++
+      this.secondsPerSolution = (
+        new Date().getTime() - startTime.getTime()
+      ) / numberSolved / 1000;
+
       this.mathForm.setValue({
         a: this.randomNumber(),
         b: this.randomNumber(),
