4e48d7ba0bacbc8c3f04955fd664d84161d63c19
beataj <beata@emc2wave.com>
2021-03-23 17:47:47 -0700

    add filter operator

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { delay } from 'rxjs/operators'
+import { delay, filter } from 'rxjs/operators'
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -27,12 +27,9 @@ export class EquationComponent implements OnInit {
 
   ngOnInit(): void {
     this.mathForm.statusChanges.pipe(
+      filter(value => value === 'VALID'),
       delay(300)
-    ).subscribe((value) => {
-      if(value === 'INVALID') {
-        return;
-      }
-
+    ).subscribe(() => {
       this.mathForm.setValue({
         a: this.randomNumber(),
         b: this.randomNumber(),
